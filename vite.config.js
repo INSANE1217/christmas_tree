a1be2d0a1fed23cf379b351a71074393a46abc7a
이원빈 <108224531+INSANE1217@users.noreply.github.com>
2025-12-25 17:08:11 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,5 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
 
 export default defineConfig({
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'index.npm.js'),
-      name: 'ChristmasTree',
-      fileName: 'index',
-      formats: ['es', 'cjs']
-    },
-    rollupOptions: {
-      external: ['firebase', 'firebase/app', 'firebase/firestore', 'firebase/analytics'],
-    }
-  }
+  base: '/christmas_tree/', // ⭐ 레포 이름 정확히
 });
